65f5dc80778ec43e5d63264d43e7d3f4b02057d1
Jaime Daza <jaime.daza@multicaja.cl>
2022-05-06 16:50:05 -0400

    first commit

---
+++ src/test/java/com/example/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.Assert.assertTrue;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+        assertTrue(Boolean.TRUE);
+    }
+
+}
